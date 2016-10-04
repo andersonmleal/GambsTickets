20dfaa4c1f7d62d2e27a9d4f024964dc79920bf0
anderson.mleal <anderson.mleal@CAS555012W0716.senacsp.edu.br>
2016-10-03 22:20:56 -0300

    Tela carrinho de compra

---
+++ src/main/java/bean/CarrinhoManagedBean.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import entidade.Evento;
+import entidade.Setor;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Leal
+ */
+@Named(value = "CarrinhoManagedBean")
+@SessionScoped
+public class CarrinhoManagedBean implements Serializable {
+
+    private ArrayList<Evento> eventos;
+
+    public CarrinhoManagedBean() {
+
+        eventos = new ArrayList<>();
+
+    }
+
+    public boolean addCarrinho(Evento ev) {
+
+        try {
+            eventos.add(ev);
+            return true;
+        } catch (Exception e) {
+
+            return false;
+        }
+
+    }
+
+    /**
+     * @return the eventos
+     */
+    public ArrayList<Evento> getEventos() {
+        return eventos;
+    }
+
+    /**
+     * @param eventos the eventos to set
+     */
+    public void setEventos(ArrayList<Evento> eventos) {
+        this.eventos = eventos;
+    }
+
+    public int getTamanhoArray() {
+        return eventos.size();
+    }
+
+
+}
