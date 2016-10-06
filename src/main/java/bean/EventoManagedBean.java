cdd0307e5848e96c71e672ee22dc1eec8b117f49
anderson.leal <anderson.leal@ITG012.itg.intranet>
2016-10-06 17:07:17 -0300

    ajuste telas

--- src/main/java/bean/EventoManagedBean.java
+++ src/main/java/bean/EventoManagedBean.java
@@ -32,10 +32,10 @@ public class EventoManagedBean implements Serializable {
         Evento ev = new Evento();
 
         ev.setId_evento(0);
-        ev.setNome_evento("NovoNome1");
+        ev.setNome_evento("Metallica");
         ev.setCaminhoImagem("img/01.jpg");
         ev.setCaminhoImagemBack("img/01_back.jpg");
-        ev.setDescricao("Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ideias de um autor, que serão interpretadas pelo leitor de acordo com seus conhecimentos linguísticos e culturais. Seu tamanho é variável.");
+        ev.setDescricao("Metallica é uma banda norte-americana de heavy metal originaria de Los Angeles, mas com base em San Francisco. Os seus lançamentos incluem tempos rápidos, instrumentais, e musicalidade agressiva, a qual os colocou no lugar de uma das bandas fundadoras do Big Four of Thrash, conjuntamente com Slayer, Megadeth e Anthrax. O Metallica foi formado em 1981, após James Hetfield responder a um anúncio que Lars Ulrich colocou no jornal local. A sua formação atual apresenta os fundadores Ulrich (bateria) e Hetfield (vocal e guitarra base), o guitarrista Kirk Hammett (que se juntou à banda em 1983), e o baixista Robert Trujillo (membro desde 2003). Antes de chegarem à sua formação atual, a banda teve outros integrantes, sendo eles: Dave Mustaine (guitarra), Ron McGovney, Cliff Burton e Jason Newsted (baixo).");
         
         Setor set = new Setor("Pista", 150.00, 5000);
         ev.addSetores(set);
@@ -50,10 +50,10 @@ public class EventoManagedBean implements Serializable {
         Evento ev2 = new Evento();
 
         ev2.setId_evento(2);
-        ev2.setNome_evento("NovoNome2");
+        ev2.setNome_evento("CPM 22");
         ev2.setCaminhoImagem("img/02.png");
         ev2.setCaminhoImagemBack("img/02_back.jpg");
-        ev2.setDescricao("descricao_2");
+        ev2.setDescricao("CPM 22 é uma banda brasileira de hardcore melódico formada em 1995 na cidade de Barueri, São Paulo. Os membros, Badauí (vocal), Japinha (bateria), Luciano (guitarra), Fernando (baixo) e Phil (guitarra) já abriram shows de bandas internacionais como Lagwagon, No Fun at All, Down by Law e System of a Down (Rock in Rio 6, onde comemoravam 20 anos da banda). Foi uma das poucas bandas brasileiras de hardcore a ganhar um disco de ouro e fazer sucesso no mainstream, e com isso, abriu as portas para uma nova geração de bandas brasileiras de rock. Em 2008 ganharam um Grammy Latino de melhor álbum de rock brasileiro.");
         
         Setor set4 = new Setor("Pista Premium", 300.00, 5000);
         ev2.addSetores(set4);
@@ -67,10 +67,10 @@ public class EventoManagedBean implements Serializable {
         Evento ev3 = new Evento();
 
         ev3.setId_evento(3);
-        ev3.setNome_evento("NovoNome3");
+        ev3.setNome_evento("Natiruts");
         ev3.setCaminhoImagem("img/03.png");
         ev3.setCaminhoImagemBack("img/03_back.jpg");
-        ev3.setDescricao("descricao_3");
+        ev3.setDescricao("Natiruts é uma banda brasileira de reggae pop formada em Brasília em 1996. Chamada inicialmente Nativus, a banda de reggae foi rebatizada de Natiruts devido a um grupo catarinense de música regional, Os Nativos, que entrou com um processo. A banda brasíliense defende o reggae de raiz mas incorporou ao som uma grande influência brasileira. Quando ainda chamava-se Nativus, o grupo vendeu 40 mil discos independentes com o sucesso \"Presente de um beija-flor\", até ser contratada pela EMI. A nova edição do disco, Nativus, vendeu 450 mil cópias. O segundo disco, Povo Brasileiro, foi produzido por Liminha e, como o reggae de Bob Marley, tem músicas com mensagens de alto teor político, como \"Proteja-se e lute\" e \"Povo brasileiro\".");
         
         Setor set7 = new Setor("Pista", 220.00, 5000);
         ev3.addSetores(set7);
