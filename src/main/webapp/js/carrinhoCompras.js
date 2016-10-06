60495e509d2e27c11604ec23e60d36c6120e043f
Guilherme Gomes <silvana@silvana-PC>
2016-10-06 01:24:08 -0300

    Tela carrinho de compras.

--- src/main/webapp/js/carrinhoCompras.js
+++ src/main/webapp/js/carrinhoCompras.js
@@ -3,3 +3,29 @@ function iniciar() {
     $(".glyphicon-shopping-cart").addClass("glyphiconSelecionado");
     
 }
+function calcValor(e, campo) {
+    if (SomenteNumero(e)) {
+        qtd = document.getElementById("quantidade").value;
+        var whichCode = (window.Event) ? e.which : e.keyCode;
+        qtd += String.fromCharCode(whichCode);
+
+        preco = campo.selectedOptions[0].getAttribute("data-preco");
+        result = parseFloat(preco * qtd).toFixed(2);
+            document.getElementById("valor").value = result.replace(".", ",");
+    }else{
+        limpaCampos();
+        return false;
+    }
+
+}
+function SomenteNumero(e) {
+    var tecla = (window.event) ? event.keyCode : e.which;
+    if ((tecla > 47 && tecla < 58))
+        return true;
+    else {
+        if (tecla == 8 || tecla == 0)
+            return true;
+        else
+            return false;
+    }
+}
