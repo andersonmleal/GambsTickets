f2c1305b6a850c0c14d4941efb80b6a9ec9b60ff
Gustavo <Gustavo@GUSTAVO>
2016-11-26 15:42:17 -0200

    Montar query da consulta.

---
+++ src/main/java/jpa/GraficoJPA.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class GraficoJPA {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("DataTicket");
+    EntityManager em = emf.createEntityManager();
+
+    public GraficoJPA() {
+
+    }
+
+    public float faturamento(int mes, int ano) {
+        float total;
+        //Monta query da consulta
+        String query = "SELECT sum(QUANTIDADE) FROM APP.VENDA WHERE DT_CADASTRO between '2016-11-01' and '2016-11-31';";
+        Query qr = em.createQuery(query);
+        total = (float) qr.getSingleResult();
+
+        return total;
+    }
+
+}
