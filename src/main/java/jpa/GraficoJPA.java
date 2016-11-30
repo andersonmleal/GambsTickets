d80101fce8a7625c6f8f70b25372a1cbafbc50e1
Guilherme Gomes <silvana@silvana-PC>
2016-11-30 01:38:40 -0200

    Select para gráfico.

--- src/main/java/jpa/GraficoJPA.java
+++ src/main/java/jpa/GraficoJPA.java
@@ -5,9 +5,13 @@
  */
 package jpa;
 
+import entidade.Venda;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -25,20 +29,36 @@ public class GraficoJPA {
 
     }
 
-    public float faturamento(int mes, int ano) throws ParseException {
+    public float faturamento(int mes, int ano) {
+        float resultado = 0;
         EntityManager em = emf.createEntityManager();
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        //DATAS HARDCODE
         String dtInicial = "01/11/2016";
         String dtFinal = "30/11/2016";
         //CONVERTE STRING NO FORMATO dd/MM/yyyy PARA SQL DATA.
-        java.sql.Date dataIni = new java.sql.Date(format.parse(dtInicial).getTime());
-        java.sql.Date dataFin = new java.sql.Date(format.parse(dtFinal).getTime());
+        java.sql.Date dataIni = null;
+        java.sql.Date dataFin = null;
+        try {
+            dataIni = new java.sql.Date(format.parse(dtInicial).getTime());
+            dataFin = new java.sql.Date(format.parse(dtFinal).getTime());
+        } catch (ParseException ex) {
+            Logger.getLogger(GraficoJPA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         try {
             // Query JPQL
-            Query query = em.createQuery("select sum(v.quantidade) from Venda v WHERE v.dt_cadastro >= :dataInicial and v.dt_cadastro <= :dataFinal");
+            Query query = em.createQuery("select v from Venda v WHERE v.dt_cadastro >= :dataInicial and v.dt_cadastro <= :dataFinal");
             query.setParameter("dataInicial", dataIni);
             query.setParameter("dataFinal", dataFin);
-            float resultado = (float) query.getSingleResult();
+
+            List<Venda> vendas = query.getResultList();
+            if (vendas.size() > 0) {
+                for (int i = 0; i < query.getResultList().size(); i++) {
+                    resultado = resultado + vendas.get(i).getQuantidade();
+
+                }
+            }
             return resultado;
         } finally {
             em.close();
