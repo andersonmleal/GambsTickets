ef75124691e588eedb3d2a811b3071a9b638fc00
guilh <guilh@LAPTOP-3MCFM8QS>
2016-12-04 10:51:45 -0200

    Cadastro de telefone

--- src/main/java/bean/UsuarioBean.java
+++ src/main/java/bean/UsuarioBean.java
@@ -32,7 +32,9 @@ public class UsuarioBean implements Serializable {
     private Usuario usuario;
     private Endereco endereco;
     private UsuarioJPA usuarioJPA;
-    private String telefone[];
+    private String telefone;
+    private String telefoneComercial;
+    private String telefoneCelular;
     private String dtNasc;
 
     public Usuario getUsuario() {
@@ -41,6 +43,38 @@ public class UsuarioBean implements Serializable {
 
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
+    }
+
+    public UsuarioJPA getUsuarioJPA() {
+        return usuarioJPA;
+    }
+
+    public void setUsuarioJPA(UsuarioJPA usuarioJPA) {
+        this.usuarioJPA = usuarioJPA;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTelefoneComercial() {
+        return telefoneComercial;
+    }
+
+    public void setTelefoneComercial(String telefoneComercial) {
+        this.telefoneComercial = telefoneComercial;
+    }
+
+    public String getTelefoneCelular() {
+        return telefoneCelular;
+    }
+
+    public void setTelefoneCelular(String telefoneCelular) {
+        this.telefoneCelular = telefoneCelular;
     }
 
     public String getDtNasc() {
@@ -59,18 +93,9 @@ public class UsuarioBean implements Serializable {
         this.endereco = endereco;
     }
 
-    public String[] getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String[] telefone) {
-        this.telefone = telefone;
-    }
-
     public UsuarioBean() {
         usuario = new Usuario();
         endereco = new Endereco();
-        telefone = new String[3];
     }
 
     public String salvar() {
@@ -105,8 +130,15 @@ public class UsuarioBean implements Serializable {
         endereco.setUsuario_evento(usuario);
         EnderecoJPA enderecoJPA = new EnderecoJPA();
         enderecoJPA.incluir(endereco);
-        cadastrarTelefone(c);
-
+        if (telefone != null || !telefone.equals("")) {
+            cadastrarTelefone(c, telefone, "Telefone Residencial");
+        }
+        if (telefoneComercial != null || !telefoneComercial.equals("")) {
+            cadastrarTelefone(c, telefoneComercial, "Telefone Comercial");
+        }
+        if (telefoneCelular != null || !telefoneCelular.equals("")) {
+            cadastrarTelefone(c, telefoneCelular, "Telefone Celular");
+        }
         // Montar mensagem a ser apresentada para usuario
         //Flash mensagem = FacesContext.getCurrentInstance().getExternalContext().getFlash();
         //mensagem.put("mensagem", new Mensagem("Cadastro realizado com sucesso", "success"));
@@ -123,28 +155,16 @@ public class UsuarioBean implements Serializable {
         }
     }
 
-    public void cadastrarTelefone(Calendar c) {
+    public void cadastrarTelefone(Calendar c, String tel, String descricao) {
         Telefone regTelefone = new Telefone();
         TelefoneJPA telefoneJPA = new TelefoneJPA();
-        for (int contador = 0; contador < telefone.length; contador++) {
-            if (telefone[contador] != null) {
-                switch (contador) {
-                    case 0:
-                        regTelefone.setDescricao("Telefone Residencial");
-                        break;
-                    case 1:
-                        regTelefone.setDescricao("Telefone Comercial");
-                        break;
-                    case 2:
-                        regTelefone.setDescricao("Telefone Celular");
-                        break;
-                }
-                regTelefone.setNumero(Long.parseLong(telefone[contador]));
-                regTelefone.setUsuario(usuario);
-                regTelefone.setDt_cadastro(c.getTime());
-                telefoneJPA.incluir(regTelefone);
-            }
-        }
+
+        regTelefone.setDescricao(descricao);
+        regTelefone.setNumero(Long.parseLong(tel));
+        regTelefone.setUsuario(usuario);
+        regTelefone.setDt_cadastro(c.getTime());
+        telefoneJPA.incluir(regTelefone);
+
     }
 
     public String removeUsuario() {
